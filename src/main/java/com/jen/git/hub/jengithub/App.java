6240bc9c161558fe594d9af7ac3b815c4d7c2377
arul murugan <malaiminnal2015@gmail.com>
2022-12-06 20:22:12 -0500

    adding the project

---
+++ src/main/java/com/jen/git/hub/jengithub/App.java
@@ -0,0 +1,13 @@
+package com.jen.git.hub.jengithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
